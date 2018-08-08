792570a51ad7d283b794f4d0376fb687d0ea74c8
Erik <erik.steinman@hotmail.com>
2018-08-08 12:06:47 +0200

    add performance_data_controller, configured route and add spec and testing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
-    namespace :v1 do
+    namespace :v1, defaults: { format: :jsonn} do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+      resources :performance_data, only: [:create]
     end
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
